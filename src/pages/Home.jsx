e84f8b4e21e53a8111513b57901803d9d300bf22
sleepyArpan <arpanchattopadhyay23@gmail.com>
2020-07-08 21:26:35 +0530

    Home component styling done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { Accent, StyledTitle } from '../styled/Random';
+import CTA from '../styled/CTA';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <StyledTitle>Ready to start typing?</StyledTitle>
+      <CTA to='/game'>
+        Click or type '<Accent>s</Accent>' to start playing
+      </CTA>
+    </div>
+  );
 };
 
 export default Home;
--- src/styled/Random.js
+++ src/styled/Random.js
@@ -3,3 +3,10 @@ import styled from 'styled-components';
 export const Accent = styled.span`
   color: #e16365;
 `;
+
+export const StyledTitle = styled.h1`
+  font-size: 4.2rem;
+  text-align: center;
+  margin-bottom: 2rem;
+  margin-top: 3rem;
+`;
