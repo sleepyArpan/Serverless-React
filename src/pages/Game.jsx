57661fd938f1f4a55afc58add1d6a6533594b36d
sleepyArpan <arpanchattopadhyay23@gmail.com>
2020-07-10 18:24:29 +0530

    Playing around with hooks and timer

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyledGame,
   StyledScore,
   StyledTimer,
   StyledCharacter,
 } from '../styled/Game';
-
 import { Strong } from '../styled/Random';
 
 const Game = () => {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setScore((prevScore) => prevScore + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [score]);
+
   return (
     <StyledGame>
       <StyledScore>
-        Score:<Strong>0</Strong>
+        Score:<Strong>{score}</Strong>
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
