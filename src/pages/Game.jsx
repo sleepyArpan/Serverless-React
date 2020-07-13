dd88038eac4a5e76064381a94353c04163afec96
sleepyArpan <arpanchattopadhyay23@gmail.com>
2020-07-13 09:59:14 +0530

    Timer implementation done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -5,28 +5,61 @@ import {
   StyledTimer,
   StyledCharacter,
 } from '../styled/Game';
-import { Strong } from '../styled/Random';
+import { Strong, Accent } from '../styled/Random';
 
-const Game = () => {
+const Game = ({ history }) => {
   const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [score]);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+    //eslint-disable-next-line
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMsString = ('0000' + msPassedStr).slice(-5); // 00000 - first 2 seconds, last 3 are the ms that have passed
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMsString.substring(formattedMsString.length - 3));
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = '';
+    for (let i = 0; i < length; i++) {
+      zeros += '0';
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push('/gameover');
+    }
+  }, [seconds, history]);
 
   return (
     <StyledGame>
       <StyledScore>
         Score:<Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>
+        <Accent>A</Accent>
+      </StyledCharacter>
       <StyledTimer>
-        Time: <Strong>00: 000</Strong>
+        Time:{' '}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
